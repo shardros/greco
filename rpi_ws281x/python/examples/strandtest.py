48f8a513d58c0d4cbbfab999c7b98019a9be7cc3
shardros <shardros@gmail.com>
2018-02-04 16:45:41 +0000

    Debugging

--- rpi_ws281x/python/examples/strandtest.py
+++ rpi_ws281x/python/examples/strandtest.py
@@ -24,12 +24,12 @@ def opt_parse():
                 signal.signal(signal.SIGINT, signal_handler)
  
 # LED strip configuration:
-LED_COUNT      = 8      # Number of LED pixels.
+LED_COUNT      = 25      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 25      # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 125      # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_RGB   # Strip type and colour ordering
